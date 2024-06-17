c9880bfeb5828af5c0657343c61dee16bf43c5a2
Alisson Augusto <alisson07a@hotmail.com>
2024-06-17 00:02:27 -0300

    implementações aula 10/06

--- src/app/usuario/page.tsx
+++ src/app/usuario/page.tsx
@@ -1,13 +1,53 @@
+"use client" //componente será renderizado apenas no lado Cliente da aplicação
+
 import Botao from "@/components/Botao";
 import Titulo from "@/components/Titulo";
 import TabelaUsuario from "@/components/Usuario/TabelaUsuario";
+import Usuario from "@/core/Entidade/Usuario";
+import { useEffect, useState } from "react";
 
 export default function PaginaUsuario() {
+    /*
+    const usuarios = [
+        new Usuario("Fernando", "fernando@gaffo", "gaffo", "aaa", 'ADMIN', 1),
+        new Usuario("jose", "jose@couve", "josedascouve", "aaa", 'ADMIN', 2),
+        new Usuario("Fernando2", "fernando@gaffo2", "gaffo2", "aaa", 'ADMIN', 3)
+    ];
+    */
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    useEffect(() => {
+        async function getUsuarios() {
+            const response = await fetch("http://localhost:8080/usuarios");
+            const data = await response.json();
+            setUsuarios(data);
+        }
+
+        getUsuarios();
+    });
+
+    function usuarioSelecionado(usuario: Usuario) {
+        console.log("Selecionar", usuario.nome)
+    }
+
+    async function usuarioExcluido(usuario: Usuario) {
+        await fetch(`http://localhost:8080/usuarios/${usuario.id}`, { method: "DELETE" });
+
+        setUsuarios(
+            usuarios.filter((tmpUsuario) => {
+                tmpUsuario.id !== usuario.id;
+            })
+        )
+    }
+
     return (
         <div>
             <Titulo texto="Cadastro de Usuário" />
             <Botao>Novo Usuário</Botao>
-            <TabelaUsuario />
+            <TabelaUsuario usuarios={usuarios}
+                usuarioSelecionado={usuarioSelecionado}
+                usuarioExcluido={usuarioExcluido} />
         </div>
     );
 }
--- src/components/Usuario/TabelaUsuario.tsx
+++ src/components/Usuario/TabelaUsuario.tsx
@@ -1,31 +1,51 @@
+"use cliente" //componente será renderizado apenas no lado Cliente da aplicação
+
+import Usuario from "@/core/Entidade/Usuario";
 import { IconeEditar, IconeExcluir } from "../Icones";
 
-export default function TabelaUsuario() {
+interface TabelaProps {
+    usuarios: Usuario[];
+    usuarioSelecionado?: (usuario: Usuario) => void;
+    usuarioExcluido?: (usuario: Usuario) => void;
+}
+
+export default function TabelaUsuario(props: TabelaProps) {
+    function cabecalho() {
+        return (<tr>
+            <th className="p-3 text-center">Id</th>
+            <th className="p-3 text-left">Nome</th>
+            <th className="p-3 text-left">E-mail</th>
+            <th className="p-3 text-center">Usuário</th>
+            <th className="p-3 text-center">Nível Acesso</th>
+            <th className="p-3 text-center">Ações</th>
+        </tr>);
+    }
+
+    function acoes(usuario: Usuario) {
+        return (<td className="p-3 text-center w-1/6">
+            {props.usuarioSelecionado ? (<button onClick={() => props.usuarioSelecionado?.(usuario)} className="text-green-600 rounded-full p-2 m-1 hover:bg-gray-100">{IconeEditar}</button>) : (false)}
+            {props.usuarioExcluido ? (<button onClick={() => props.usuarioExcluido?.(usuario)} className="text-red-600 rounded-full p-2 m-1 hover:bg-gray-100">{IconeExcluir}</button>) : (false)}
+        </td>);
+    }
+
+    function dados() {
+        return props.usuarios.map((usuario, index) => {
+            return (<tr key={usuario.id}>
+                <td className="p-3 text-center w-1/6">{usuario.id}</td>
+                <td className="p-3 text-left w-auto">{usuario.nome}</td>
+                <td className="p-3 text-left w-auto">{usuario.email}</td>
+                <td className="p-3 text-center w-auto">{usuario.usuario}</td>
+                <td className="p-3 text-center w-1/6">{usuario.nivel_acesso}</td>
+                {acoes(usuario)}
+            </tr>);
+
+        });
+    }
+
     return (
         <table className="w-full rounded-xl overflow-hidden">
-            <thead className="text-gray-200 bg-gray-700">
-                <tr>
-                    <th className="p-3 text-center">Id</th>
-                    <th className="p-3 text-left">Nome</th>
-                    <th className="p-3 text-left">E-mail</th>
-                    <th className="p-3 text-center">Usuário</th>
-                    <th className="p-3 text-center">Nível Acesso</th>
-                    <th className="p-3 text-center">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className="p-3 text-center w-1/6">1</td>
-                    <td className="p-3 text-left w-auto">a</td>
-                    <td className="p-3 text-left w-auto">b</td>
-                    <td className="p-3 text-center w-auto">c</td>
-                    <td className="p-3 text-center w-1/6">d</td>
-                    <td className="p-3 text-center w-1/6">
-                        <button className="text-green-600 rounded-full p-2 m-1 hover:bg-gray-100">{IconeEditar}</button>
-                        <button className="text-red-600 rounded-full p-2 m-1 hover:bg-gray-100">{IconeExcluir}</button>
-                    </td>
-                </tr>
-            </tbody>
+            <thead className="text-gray-200 bg-gray-700">{cabecalho()}</thead>
+            <tbody>{dados()}</tbody>
         </table>
     );
 }
